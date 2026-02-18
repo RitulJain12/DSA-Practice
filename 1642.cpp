05381fffeeda40ad555bac2af91c29d94757cc50
Ritul Jain <rituljain202@gmail.com>
2026-02-18 08:49:04 +0530

    op

--- 1642.cpp
+++ 1642.cpp
@@ -13,21 +13,20 @@ class Solution {
                 while(i+1<n &&heights[i]>=heights[i+1]) i++;
                 if(i+1==n) return i;
                  int diff=heights[i+1]-heights[i];
-                 if(bricks-diff>=0)
-                 {
+            
                     pq.push(diff);
                     bricks-=diff;
-                    i++;
-                 }
-                 else{
-                   if(ladders==0) return i;
-                   if(pq.top()+bricks<diff){
-                      bricks+=pq.top();pq.pop();
-                       ladders--;
-                   }
-                  
+                    
+                
+                if(bricks<0){
+                  if(ladders==0) return i;
+                    bricks+=pq.top();
+                    pq.pop();
+                    ladders--;
                  
                  }
+                    i++;
+
                 }  
                  return n-1;
         }
@@ -35,4 +34,3 @@ class Solution {
 
     // we simply need to use bricks untill it become zero and then we need top difference so far we used to we can replace that bricks with ladder and  we can use those bricks for current difference and if we have no ladder left then we can not move forward and return the index of current building
 
-    
