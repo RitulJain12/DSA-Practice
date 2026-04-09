b9cfde6392468dc8fc28a3aaa0aeb2e7ad552b98
Ritul Jain <rituljain202@gmail.com>
2026-04-09 21:25:44 +0530

    op

---
+++ inorderbymoristraversaol.cpp
@@ -0,0 +1,46 @@
+//Inorder traversal of a binary tree is to traverse the left subtree first, then visit the root node, and finally traverse the right subtree.
+//Morris Traversal is a tree traversal algorithm that uses O(1) space complexity by
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+vector<int> morrisInorderTraversal(TreeNode* root) {
+    vector<int> result;
+    TreeNode* current = root;
+    TreeNode* pre;
+
+    while (current) {
+        if (!current->left) {
+            result.push_back(current->val);
+            current = current->right;
+        } else {
+           
+            pre = current->left;
+            while (pre->right && pre->right != current) {
+                pre = pre->right;
+            }
+
+          
+            if (!pre->right) {
+                pre->right = current;
+                current = current->left;
+            } else {
+               
+                pre->right = NULL;
+                result.push_back(current->val);
+                current = current->right;
+            }
+        }
+    }
+    return result;
+}
