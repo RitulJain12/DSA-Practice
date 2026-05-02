0b8fee36d13f9a473fae5c6aed3da6de9599b8ca
Ritul Jain <rituljain202@gmail.com>
2026-05-02 07:52:34 +0530

    op

---
+++ bennyandodometer.cpp
@@ -0,0 +1,5 @@
+//basically its a problem which do not able to show the digit contain 3 due to which the reading comes wrong and we have to find the correct reading of the odometer
+
+
+
+//this can be done via digit dp simpy in which i need to find the count of numbers which contain 3 and then i can easily find the correct reading of the odometer by subtracting the count from the given reading
