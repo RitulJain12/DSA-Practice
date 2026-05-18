3877b89992339918a79efe4d28275ea21170f7c9
Ritul Jain <rituljain202@gmail.com>
2026-05-18 10:30:58 +0530

    op ques

---
+++ op.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    bool isSubsequence(string &word, vector<int> pos[]) {
+
+        int prev = -1;
+
+        for(char ch : word) {
+
+            vector<int> &v = pos[ch - 'a'];
+
+            auto it = upper_bound(v.begin(), v.end(), prev);
+
+            if(it == v.end()) return false;
+
+            prev = *it;
+        }
+
+        return true;
+    }
+
+    int numMatchingSubseq(string s, vector<string>& words) {
+
+        vector<int> pos[26];
+
+        for(int i = 0; i < s.size(); i++) {
+            pos[s[i] - 'a'].push_back(i);
+        }
+
+        int ans = 0;
+
+        for(auto &word : words) {
+
+            if(isSubsequence(word, pos))
+                ans++;
+        }
+
+        return ans;
+    }
+};
